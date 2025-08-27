dd3ea3d2f8b2b319ee27be7c31da73c2c1aef10a
yuhan3000 <cyuhan3000@gmail.com>
2025-08-27 12:13:26 +0800

    Update HelloBank.java

--- src/main/java/com/uob/HelloBank.java
+++ src/main/java/com/uob/HelloBank.java
@@ -1,3 +1,4 @@
+/*
 package com.uob;
 
 public class HelloBank {
@@ -6,4 +7,25 @@ public class HelloBank {
         System.out.println("Welcome to UOB!");
         System.out.println("Welcome to UOBx!");
     }
+}*/
+
+
+package com.uob;
+
+public class HelloBank {
+    public static void main(String[] args) {
+        // VULNERABILITY: Hardcoded password used for login
+        String password = "abc123"; 
+
+        // Simulate authentication (this triggers SonarCloud vulnerability detection)
+        if ("user1".equals("user1") && password.equals("abc123")) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Login failed!");
+        }
+
+        System.out.println("Welcome to UOB!");
+        System.out.println("Welcome to UOBx!");
+    }
 }
+
